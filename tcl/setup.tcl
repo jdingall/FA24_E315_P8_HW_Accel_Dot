e8944dfcf6efd333648afaafe0dc6c6eceb6d50a
Andrew Lukefahr <lukefahr@iu.edu>
2022-11-17 14:43:58 -0500

    updates for 2022

--- tcl/setup.tcl
+++ tcl/setup.tcl
@@ -17,9 +17,15 @@ add_files ${SRCDIR}/axis_fadd.sv
 
 add_files ${SRCDIR}/bd_fadd/bd_fadd.bd
 add_files ${SRCDIR}/bd_fmac/bd_fmac.bd
+add_files ${SRCDIR}/bd_fpga/bd_fpga.bd
+
 make_wrapper -files [get_files *.bd] -top
 add_files ${SRCDIR}/bd_fadd/hdl/bd_fadd_wrapper.v
 add_files ${SRCDIR}/bd_fmac/hdl/bd_fmac_wrapper.v
+add_files ${SRCDIR}/bd_fpga/hdl/bd_fpga_wrapper.v
+
+#set top
+set_property top bd_fpga_wrapper [current_fileset]
 
 # add testbenches
 create_fileset -simset sim_dot
@@ -35,6 +41,10 @@ set_property file_type SystemVerilog [get_files *.sv]
 
 # set active simulation
 current_fileset -simset [ get_filesets sim_dot_20_10 ]
+
+#seems to help surpress warning messages
+update_compile_order -fileset sources_1
+update_module_reference bd_fpga_axis_dot_20_10_0_0
 
 #launch_runs synth_1
 #wait_on_run synth_1
